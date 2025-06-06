5a5b770e6cad12f78fbe9daf2b6e3c848c903c20
Your Name <your.email@example.com>
2025-06-07 01:49:12 +0900

    Configure for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'export',
-  basePath: '/teacher-personal-branding',
   images: {
     unoptimized: true,
-  },
+  }
 }
 
 export default nextConfig
